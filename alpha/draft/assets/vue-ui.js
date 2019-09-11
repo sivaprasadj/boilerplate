8164f8f8b95405ac01c278d6cb8b128a341c2a02
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2019-09-11 12:24:25 +0900

    commit

--- alpha/draft/assets/vue-ui.js
+++ alpha/draft/assets/vue-ui.js
@@ -11,56 +11,95 @@
     rtrim: function(value) {
       return typeof value == 'string'?
           value.replace(this.rtrimRe,'') : value;
+    },
+    numRe: /^([\+\-]?)([0-9]+)(\.[0-9]+)?$/,
+    isNumber: function(value) {
+      return value.match(this.numRe);
     }
   };
 
   var components = {};
+  var mixins = {};
 
   components['mycomp'] ={
       template: '<div class="mycomp"></div>'
   };
 
-  components['number-input'] ={
-      template: '<input type="text" class="number-input"' +
-        ' @input="inputHandler"' +
-        ' @blur="blurHandler"' +
-        ' />',
-      props: {
-        value: { type: Number, default: null }
+  mixins['input-base'] ={
+    template: '<input type="text"' +
+      ' :class="styleClass"' +
+      ' @input="inputHandler"' +
+      ' @focus="focusHandler"' +
+      ' @blur="blurHandler"' +
+      ' />',
+    props: { value : {} },
+    data: function() {
+      return {
+        _value: null,
+        styleClass: '',
+        inputOpts: {
+          format: function(value) {
+            return value === null? '' : '' + value;
+          },
+          parse: function(dispVal) {
+            dispVal = util.trim(dispVal);
+            return dispVal.length == 0? null : dispVal;
+          },
+          // test if can parse.
+          isValid: function(dispVal) {
+            return true;
+          }
+        }
+      };
+    },
+    watch: {
+      value: function(newVal) {
+        this.setValue(newVal);
+      }
+    },
+    methods: {
+      setValue: function(value) {
+        this.$el.value = this.inputOpts.format(this._value = value);
       },
-      data: function() {
-        return {
-          lastValue: null
-        };
-      },
-      watch: {
-        value : function(newVal) {
-          this.$el.value = this.format(this.lastValue = newVal);
+      inputHandler: function(event) {
+        var value = this.$el.value;
+        if (this.inputOpts.isValid(value) ) {
+          this.$emit('input', this.inputOpts.parse(value) );
         }
       },
-      methods: {
+      focusHandler: function(event) {
+      },
+      blurHandler: function(event) {
+        var value = this.$el.value;
+        if (!this.inputOpts.isValid(value) ) {
+          // revert
+          this.setValue(this._value);
+        }
+      }
+    },
+    mounted: function() {
+      this.setValue(this.value);
+    }
+  };
+
+  components['number-input'] ={
+    mixins: [ mixins['input-base'] ],
+    beforeMount: function() {
+      this.styleClass = 'number-input';
+      this.inputOpts = {
         format: function(value) {
           return value === null? '' : '' + value;
         },
-        parse: function(value) {
-          value = util.trim(value);
-          return value.length == 0? null : +value;
+        parse: function(dispVal) {
+          dispVal = util.trim(dispVal);
+          return dispVal.length == 0? null : +dispVal;
         },
-        isValid: function(value) {
-          // test if can parse.
-          value = util.trim(value);
-          return value.length == 0 || !isNaN(+value);
-        },
-        inputHandler: function(event) {
-          var value = this.$el.value;
-          if (this.isValid(value) ) {
-            this.$emit('input', this.lastValue = this.parse(value) );
-          }
-        },
-        blurHandler: function(event) {
-          this.$el.value = this.format(this.lastValue);
+        isValid: function(dispVal) {
+          dispVal = util.trim(dispVal);
+          return dispVal.length == 0 || util.isNumber(dispVal);
         }
-      }
+      };
+    }
   };
 
   !function() {
