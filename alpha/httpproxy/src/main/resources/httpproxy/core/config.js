f20bcf8eb8e72d5c093c14064521da8dfb401cce
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2019-10-18 14:27:11 +0900

    commit

--- alpha/httpproxy/src/main/resources/httpproxy/core/config.js
+++ alpha/httpproxy/src/main/resources/httpproxy/core/config.js
@@ -34,11 +34,25 @@ init({
     },
     getproxy: function(event) {
       var detail = event.getDetail();
+      var console = detail.get('console');
       detail.put('proxy', 'DIRECT');
     },
     beforerequest: function(event) {
+      var detail = event.getDetail();
+      var console = detail.get('console');
     },
     beforeresponse: function(event) {
+      var detail = event.getDetail();
+      var console = detail.get('console');
+      var responseHeader = detail.get('responseHeader');
+      var cookies = responseHeader.getHeaderValues('set-cookie');
+      for (var i = 0; i < cookies.length; i += 1) {
+        var cookie = cookies[i].match(/^([^=;]+)=([^=;]*)(;.*)?$/);
+        if (cookie) {
+          var name = cookie[1];
+          var value = cookie[2];
+        }
+      }
     }
   }
 });
