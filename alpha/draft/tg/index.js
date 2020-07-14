fa0f91871f7c3e6a5cd2489447dc64eebbb9d539
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2020-07-14 18:50:54 +0900

    commit

--- alpha/draft/tg/index.js
+++ alpha/draft/tg/index.js
@@ -118,34 +118,41 @@
       metronomeView: {
         template:
           '<svg xmlns="http://www.w3.org/2000/svg"' +
-            ' style="x-display: none; overflow: hidden;"' +
-            ' width="220" height="90"' +
-            ' viewBox="0 0 220 90" >' +
-            '<rect fill="#c0ccc0" stroke="none"' +
-              ' x="0" y="0" width="220" height="90" />' +
-            '<circle ref="barAx" fill="#000" stroke="none" :opacity="cOp"' +
-              ' cx="110" cy="92" r="12" />' +
-            '<g v-for="bar in bars" :transform="bar.tran">' +
-              '<path ref="bars" fill="#000" stroke="none" :opacity="bar.op"' +
-                ' d="M-0.5 -15L-5 -84L0 -90L5 -84L0.5 -15Z" />' +
+              ' style="x-display: none; overflow: hidden;"' +
+              ' width="220" height="90"' +
+              ' viewBox="0 0 220 90" >' +
+            '<clipPath id="dspFrm">'+
+              '<rect fill="#000" stroke="none"' +
+              ' x="4" y="4" width="212" height="82" />' +
+            '</clipPath>' +
+            '<rect fill="#230" stroke="none" opacity="0.2"' +
+            ' x="0" y="0" width="220" height="90" />' +
+            '<g clip-path="url(#dspFrm)">' +
+              '<circle ref="barAx" fill="#000" stroke="none" :opacity="cOp"' +
+                ' cx="110" cy="90" r="12" />' +
+              '<g v-for="bar in bars" :transform="bar.tran">' +
+                '<path ref="bars" fill="#000" stroke="none" :opacity="bar.op"' +
+                  ' d="M-0.5 -16L-4.5 -80L0 -84L4.5 -80L0.5 -16Z" />' +
+              '</g>' +
+              '<circle ref="lPoint" fill="#000" stroke="none" :opacity="lOp"' +
+                ' :cx="pHGap" :cy="pVGap" :r="pRadius" />' +
+              '<circle ref="rPoint" fill="#000" stroke="none" :opacity="rOp"' +
+                ' :cx="220 - pHGap" :cy="pVGap" :r="pRadius" />' +
             '</g>' +
-            '<circle ref="lPoint" fill="#000" stroke="none" :opacity="lOp"' +
-              ' cx="25" cy="25" r="16" />' +
-            '<circle ref="rPoint" fill="#000" stroke="none" :opacity="rOp"' +
-              ' cx="195" cy="25" r="16" />' +
           '</svg>',
         props: {
           intf: { type: Object, default: null }
         },
         data: function() {
           return {
+            pHGap: 24, pVGap: 20, pRadius: 16,
             active: false, cOp: '0', lOp: '0', rOp: '0', bars: []
           };
         },
         mounted: function() {
 
           var activeOpacity = '1';
-          var defaultOpacity = '0.05';
+          var defaultOpacity = '0.02';
 
           this.cOp = this.lOp = this.rOp = defaultOpacity;
 
@@ -162,50 +169,63 @@
           }();
           this.bars = bars;
 
-          var intf, beat, tempo;
+          var beat, tempo;
 
           var frmT0 = 0, lastTs = 0, t, ts;
-          var tranT, bt, i, op;
+          var tranT, i, op;
           var cOp, lOp, rOp;
+          var intf, bi0, bi1;
+          var _barsLen = bars.length;
+          var _2barsLen = _barsLen * 2;
 
           var onframe = function(frmT) {
+
             if (this.intf) {
 
               intf = this.intf;
               ts = intf.getTimestamp();
               beat = intf.getBeat();
               tempo = intf.getTempo();
+
               if (lastTs != ts) {
                 frmT0 = frmT;
                 lastTs = ts;
               }
 
-              t =  ts + (frmT - frmT0) / 1000;
-
-              tranT = (t * tempo / 120) % 1;
-              if (tranT < 0.5) {
-                lOp = activeOpacity;
-                rOp = defaultOpacity;
+              if (ts == 0) {
+                // oops! unstable!!
               } else {
-                lOp = defaultOpacity;
-                rOp = activeOpacity;
-              }
-              cOp = activeOpacity;
 
-              if (this.lOp !== lOp) this.lOp = lOp;
-              if (this.rOp !== rOp) this.rOp = rOp;
-              if (this.cOp !== cOp) this.cOp = cOp;
+                t =  ts + (frmT - frmT0) / 1000;
+                tranT = (t * tempo / 120 + 0.75) % 1;
 
-              bt = tranT * 2;
-              if (bt >= 1) {
-                bt = 2 - bt;
+                cOp = activeOpacity;
+                if (tranT < 0.5) {
+                  lOp = activeOpacity;
+                  rOp = defaultOpacity;
+                } else {
+                  lOp = defaultOpacity;
+                  rOp = activeOpacity;
+                }
+
+                if (this.cOp !== cOp) this.cOp = cOp;
+                if (this.lOp !== lOp) this.lOp = lOp;
+                if (this.rOp !== rOp) this.rOp = rOp;
+
+                bi0 = Math.floor(tranT * _2barsLen);
+                bi1 = (bi0 + _2barsLen - 1) % _2barsLen;
+                bi0 = bi0 >= _barsLen? _2barsLen - 1  - bi0 : bi0;
+                bi1 = bi1 >= _barsLen? _2barsLen - 1  - bi1 : bi1;
+
+                for (i = 0; i < _barsLen; i += 1) {
+                  op = (i == bi0 || i == bi1)? activeOpacity : defaultOpacity;
+                  if (bars[i].op !== op) {
+                    bars[i].op = op;
+                  };
+                }
+
               }
-              for (i = 0; i < bars.length; i += 1) {
-                op = i == ~~(bt * bars.length)? activeOpacity : defaultOpacity;
-                if (bars[i].op !== op) {
-                  bars[i].op = op;
-                };
-              }
+
             } else {
               this.cOp = this.lOp = this.rOp = defaultOpacity;
               for (i = 0; i < bars.length; i += 1) {
