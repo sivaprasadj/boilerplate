a834c73f9bcf7f8b682d1082279c5c148f1fc757
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2020-07-14 14:55:28 +0900

    commit

--- alpha/draft/tg/index.js
+++ alpha/draft/tg/index.js
@@ -6,12 +6,11 @@
 
     var components = {
       metronome: {
-        template:
-          '<span style="display:none;"></span>',
+        template: '<span style="display:none;">metronome</span>',
         props: {
           beat: { type: Number, default: 4 },
-          gain: { type: Number, default: -40 },
           tempo: { type: Number, default: 120 },
+          gain: { type: Number, default: -40 },
           mute: { type: Boolean, default: false }
         },
         data: function() {
@@ -19,9 +18,6 @@
             audioContext: null,
             reset: function() {}
           };
-        },
-        watch: {
-          params: function() {}
         },
         computed: {
           params: function() {
@@ -33,13 +29,10 @@
             var numUnitsPerStep = 16 * Math.max(1, Math.ceil(120 / tempo) );
             var stepPerTime = numUnitsPerStep * tempo / 60;
             return {
-              freq: freq, beat: beat, vol: vol,
+              beat: beat, tempo: tempo, freq: freq, vol: vol,
               numUnitsPerStep: numUnitsPerStep,
               stepPerTime: stepPerTime
             }; 
-          },
-          playing: function() {
-            return !!this.audioContext;
           }
         },
         methods: {
@@ -95,8 +88,6 @@
                         this.params.freq * 2 : this.params.freq;
                     vol = step % this.params.numUnitsPerStep == 0? this.params.vol : 0;
                     lastStep = step;
-                    this.$emit('step', { step: step,
-                      beat: this.params.beat, numUnitsPerStep: this.params.numUnitsPerStep });
                   }
 
                   chData[i] = vol * wave(2 * Math.PI * freq * t);
@@ -107,12 +98,19 @@
 
               scriptNode.connect(gainNode);
               this.audioContext = audioContext;
+              var _met = this;
+              this.$emit('start', {
+                getTimestamp: function() { return t; },
+                getBeat: function() { return _met.params.beat; },
+                getTempo: function() { return _met.params.tempo; }
+              });
             }
           },
           stop: function() {
             if (this.audioContext) {
               this.audioContext.close();
               this.audioContext = null;
+              this.$emit('stop');
             }
           }
         }
@@ -136,27 +134,85 @@
             '<circle ref="rPoint" fill="#000" stroke="none" :opacity="rOp"' +
               ' cx="195" cy="25" r="16" />' +
           '</svg>',
+        props: {
+          intf: { type: Object, default: null }
+        },
         data: function() {
           return {
             active: false, cOp: '0', lOp: '0', rOp: '0', bars: []
           };
         },
         mounted: function() {
-          var defaultOpacity = '0.1';
+
+          var activeOpacity = '1';
+          var defaultOpacity = '0.05';
+
           this.cOp = this.lOp = this.rOp = defaultOpacity;
+
           var bars = [];
-          var l = 6;
-          for (var i = -l; i <= l; i += 1) {
-            bars.push({
-              n: i,
-              tran: 'translate(110 92) rotate(' + (i * 7.75) + ')',
-              op: defaultOpacity
-            });
-          }
+          !function() {
+            var l = 6;
+            for (var i = -l; i <= l; i += 1) {
+              bars.push({
+                n: i,
+                tran: 'translate(110 92) rotate(' + (i * 7.75) + ')',
+                op: defaultOpacity
+              });
+            }
+          }();
           this.bars = bars;
 
+          var intf, beat, tempo;
 
-          var onframe = function() {
+          var frmT0 = 0, lastTs = 0, t, ts;
+          var tranT, bt, i, op;
+          var cOp, lOp, rOp;
+
+          var onframe = function(frmT) {
+            if (this.intf) {
+
+              intf = this.intf;
+              ts = intf.getTimestamp();
+              beat = intf.getBeat();
+              tempo = intf.getTempo();
+              if (lastTs != ts) {
+                frmT0 = frmT;
+                lastTs = ts;
+              }
+
+              t =  ts + (frmT - frmT0) / 1000;
+
+              tranT = (t * tempo / 120) % 1;
+              if (tranT < 0.5) {
+                lOp = activeOpacity;
+                rOp = defaultOpacity;
+              } else {
+                lOp = defaultOpacity;
+                rOp = activeOpacity;
+              }
+              cOp = activeOpacity;
+
+              if (this.lOp !== lOp) this.lOp = lOp;
+              if (this.rOp !== rOp) this.rOp = rOp;
+              if (this.cOp !== cOp) this.cOp = cOp;
+
+              bt = tranT * 2;
+              if (bt >= 1) {
+                bt = 2 - bt;
+              }
+              for (i = 0; i < bars.length; i += 1) {
+                op = i == ~~(bt * bars.length)? activeOpacity : defaultOpacity;
+                if (bars[i].op !== op) {
+                  bars[i].op = op;
+                };
+              }
+            } else {
+              this.cOp = this.lOp = this.rOp = defaultOpacity;
+              for (i = 0; i < bars.length; i += 1) {
+                bars[i].op = defaultOpacity;
+              }
+            }
+
             if (this.active) {
               window.requestAnimationFrame(onframe);
             }
@@ -179,25 +235,28 @@
         gain: -30,
         tempo: 120,
         mute: false,
-        playing: false
+        playing: false,
+        intf: null
       },
       methods: {
         start_clickHandler: function() {
           var metronome = this.$refs.metronome;
-          if (!metronome.playing) {
+          if (!this.playing) {
             metronome.start();
           } else {
             metronome.stop();
           }
-          this.playing = metronome.playing;
+        },
+        metronome_startHandler: function(intf) {
+          this.playing = true;
+          this.intf = intf;
+        },
+        metronome_stopHandler: function(intf) {
+          this.playing = false;
+          this.intf = null;
         }
       },
       mounted: function() {
-        this.$refs.metronome.$on('step', function(event) {
-          if (event.step % event.numUnitsPerStep == 0) {
-            console.log(event);
-          }
-        });
       }
     })
   });
